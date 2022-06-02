9d2ba415a0e4d10da6784f0f85590bebedf27924
C-L-Driver <69204245+C-L-Driver@users.noreply.github.com>
2022-06-02 19:44:40 +0100

    finnished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMousedOver, setMousedOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMousedOver() {
+    setMousedOver(true);
+  }
+
+  function handleMousedOut() {
+    setMousedOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMousedOver}
+        onMouseOut={handleMousedOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
